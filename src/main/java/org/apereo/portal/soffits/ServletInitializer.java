f1270ca13df1c007a1cadb38e0f2ba67057d4fb0
Drew Wills <wills.drew@gmail.com>
2016-06-03 19:58:15 -0700

    New project exactly as generated by Spring Initializer (https://start.spring.io/)

---
+++ src/main/java/org/apereo/portal/soffits/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.apereo.portal.soffits;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SoffitSamplesApplication.class);
+	}
+
+}
---
+++ src/main/java/org/apereo/portal/soffits/SoffitSamplesApplication.java
@@ -0,0 +1,12 @@
+package org.apereo.portal.soffits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoffitSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoffitSamplesApplication.class, args);
+	}
+}
