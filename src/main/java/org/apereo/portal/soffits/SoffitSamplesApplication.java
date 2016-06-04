4b25f4def95f23a8d17f2805a06825862d728eeb
Drew Wills <wills.drew@gmail.com>
2016-06-04 11:21:35 -0700

    Add caching support and configs based on EhCache

--- src/main/java/org/apereo/portal/soffits/SoffitSamplesApplication.java
+++ src/main/java/org/apereo/portal/soffits/SoffitSamplesApplication.java
@@ -3,9 +3,15 @@ package org.apereo.portal.soffits;
 import org.apereo.portlet.soffit.renderer.SoffitRendererController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
+@EnableCaching
 public class SoffitSamplesApplication {
 
     public static void main(String[] args) {
@@ -18,6 +24,19 @@ public class SoffitSamplesApplication {
     @Bean
     public SoffitRendererController soffitRendererController() {
         return new SoffitRendererController();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new EhCacheCacheManager(ehCacheCacheManager().getObject());
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheCacheManager() {
+        EhCacheManagerFactoryBean cmfb = new EhCacheManagerFactoryBean();
+        cmfb.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        cmfb.setShared(true);
+        return cmfb;
     }
 
 }
