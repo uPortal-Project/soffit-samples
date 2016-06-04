237e564a0135f6e5886439493f26cd3cdbbf6bf1
Drew Wills <wills.drew@gmail.com>
2016-06-04 09:37:35 -0700

    Add support for soffits following instructions in the Soffit README

--- src/main/java/org/apereo/portal/soffits/SoffitSamplesApplication.java
+++ src/main/java/org/apereo/portal/soffits/SoffitSamplesApplication.java
@@ -1,7 +1,9 @@
 package org.apereo.portal.soffits;
 
+import org.apereo.portlet.soffit.renderer.SoffitRendererController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoffitSamplesApplication {
@@ -9,4 +11,13 @@ public class SoffitSamplesApplication {
     public static void main(String[] args) {
         SpringApplication.run(SoffitSamplesApplication.class, args);
     }
+
+    /**
+     * Enables soffits within this application.
+     */
+    @Bean
+    public SoffitRendererController soffitRendererController() {
+        return new SoffitRendererController();
+    }
+
 }
